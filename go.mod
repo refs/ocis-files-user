51fa9ce640e41e7023178c44dc53caeb84ea6e72
A.Unger <zyxancf@gmail.com>
2020-09-08 12:06:20 +0200

    make use of ocis-files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/refs/ocis-files-user
+
+go 1.15
+
+require github.com/refs/ocis-files/ocis-webdav v0.1.1
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	webdav "github.com/refs/ocis-files/ocis-webdav/pkg/server"
+)
+func main() {
+	webdav.Print()
+}
